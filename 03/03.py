bb07e8a1f73a272885980ba46ecf43c00a90fc90
Luděk Chovanec <github@chovanec.com>
2022-12-05 17:56:40 +0100

    modified

--- 03/03.py
+++ 03/03.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 total = 0
-lines = []
-
-# read lines
 
 for line in open("input.txt", "r"):
-    lines.append(line.strip())
+    
+    # split line in two
+    assert len(line) % 2 == 1
 
-# process lines
+    a = line[:len(line)/2]
+    b = line[len(line)/2:]
 
-for i in range(len(lines),3):
-    for c in lines[i]:
-        if c in lines[i+1] and c in lines[i+2]:
-            # add to total
-            if ord('a') <= ord(both) <= ord('z'):
-                total += ord(both) - ord('a') + 1
-            elif ord('A') <= ord(both) <= ord('Z'):
-                total += ord(both) - ord('A') + 27
+    both = ''
+    # go through the first half and see if in second half
+    for c in a:
+      if c in b:
+        both = c
+        break
+        
+    print(both)
+
+    # add to priority
+    if ord('a') <= ord(both) <= ord('z'):
+        total += ord(both) - ord('a') + 1
+    elif ord('A') <= ord(both) <= ord('Z'):
+        total += ord(both) - ord('A') + 27
 
 print(total)
